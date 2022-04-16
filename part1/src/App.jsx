16333f0dc4aedd95246c2ce1a5eeea7679ec3ce9
404HoneyBadger <94186840+404HoneyBadger@users.noreply.github.com>
2022-04-16 13:02:09 -0400

    second commit

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Header= (p) => {
+const Header= ({randomCourse}) => {
   return (
     <div>
-    <h1> {p.course} </h1>
+    <h1> {randomCourse} </h1>
     </div>
   )
 }
@@ -11,12 +11,12 @@ const Header= (p) => {
 const Content= (p) => {
   return (
     <div>
-      <Part part={p.part1} exercises={p.exercises1}/>
-      <Part part={p.part2} exercises={p.exercises2}/>
-      <Part part={p.part3} exercises={p.exercises3}/>
+      <Part printName={p.randomName1} printExercises={p.randomExercises1}/>
+      <Part printName={p.randomName2} printExercises={p.randomExercises2}/>
+      <Part printName={p.randomName3} printExercises={p.randomExercises3}/>
     {/* <p>{p.part1} {p.exercises1}</p>
-    <p>{p.part2} {p.exercises2}</p> 
-    <p>{p.part3} {p.exercises3}</p>  */}
+    <p>{p.myPart2} {p.exercises2}</p> 
+    <p>{p.myPart3} {p.exercises3}</p>  */}
     </div>
   )
 }
@@ -24,7 +24,7 @@ const Content= (p) => {
 const Part= (p) => {
   return (
     <div>
-    <p>{p.part} {p.exercises} </p> 
+    <p>{p.printName} {p.printExercises} </p> 
     </div>
   )
 }
@@ -32,28 +32,80 @@ const Part= (p) => {
 const Totals= (p) => {
   return (
     <div>
-    <p>Number of exercises {p.exercises1 + p.exercises2 + p.exercises3}</p>
+    <p>Number of exercises {p.whereExercises1 + p.whereExercises2 + p.whereExercises3}</p>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using p to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const myCourse = {
+    name: 'Half Stack application development',
+    arrParts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  // const myCourse = 'Half Stack application development';
+  // const arrParts = [
+  //   {
+  //     name: 'Fundamentals of React',
+  //     exercises: 10
+  //   },
+  //   {
+  //     name: 'Using props to pass data',
+  //     exercises: 7
+  //   },
+  //   {
+  //     name: 'State of a component',
+  //     exercises: 14
+  //   }
+  // ]
+
+
+  // const myPart1 = {
+  //   name: 'Fundamentals of React',
+  //   exercises: 10
+  // }
+  // const myPart2 = {
+  //   name: 'Using props to pass data',
+  //   exercises: 7
+  // }
+  // const myPart3 = {
+  //   name: 'State of a component',
+  //   exercises: 14
+  // }
   
 
   return (
     <>
-      <Header course={course} />
-      <Content part1={part1} exercises1= {exercises1} />
-      <Content part2={part2} exercises2= {exercises2} />
-      <Content part3={part3} exercises3= {exercises3} />
-      <Totals exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    <Header randomCourse={myCourse.name} />
+
+    <Content randomName1={myCourse.arrParts[0].name} randomExercises1= {myCourse.arrParts[0].exercises} />
+    <Content randomName2={myCourse.arrParts[1].name} randomExercises2= {myCourse.arrParts[1].exercises} />
+    <Content randomName3={myCourse.arrParts[2].name} randomExercises3= {myCourse.arrParts[2].exercises} />
+    <Totals whereExercises1={myCourse.arrParts[0].exercises} whereExercises2={myCourse.arrParts[1].exercises} whereExercises3={myCourse.arrParts[2].exercises} />
+
+      {/* <Header randomCourse={myCourse} />
+
+      <Content randomName1={arrParts[0].name} randomExercises1= {arrParts[0].exercises} />
+      <Content randomName2={arrParts[1].name} randomExercises2= {arrParts[1].exercises} />
+      <Content randomName3={arrParts[2].name} randomExercises3= {arrParts[2].exercises} />
+      <Totals whereExercises1={arrParts[0].exercises} whereExercises2={arrParts[1].exercises} whereExercises3={arrParts[2].exercises} /> */}
+
+      {/* <Content randomPart1={myPart1.name} randomExercises1= {myPart1.exercises} />
+      <Content randomPart2={myPart2.name} randomExercises2= {myPart2.exercises} />
+      <Content randomPart3={myPart3.name} randomExercises3= {myPart3.exercises} />
+      <Totals whereExercises1={myPart1.exercises} whereExercises2={myPart2.exercises} whereExercises3={myPart3.exercises} /> */}
      
     </>
   );
