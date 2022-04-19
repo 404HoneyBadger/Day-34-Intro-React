5ca4d210865312f18ef4e900f5df091c5bfc44f6
404HoneyBadger <94186840+404HoneyBadger@users.noreply.github.com>
2022-04-19 10:43:02 -0400

    second commit

--- part3/src/App.jsx
+++ part3/src/App.jsx
@@ -12,29 +12,28 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
   ]
 
-  const anecdoteArr= anecdotes.length
-  
-  const emptyArr = new Array(anecdoteArr).fill(0)//METHOD Changes all array elements from start to end index to a static value and returns the modified array
+  const anecdoteArr= anecdotes.length // This sets the 'ancedote' array length and turns the vairable into a number. The 'anecdoteArr' is used to make a new array called 'emptyArr'
 
-  const [selected, setSelected] = useState(0) //lms
+  const emptyArr = new Array(anecdoteArr).fill(0)// you got the length of 'anecdotes', you passed it to 'new Array' to make another array with the same length, and you used '.fill()' METHOD to fill it up with 0s. The .fill() METHOD changes all array elements. 'emptyArr'is now a variable holding an array of all 0s. This will be an argument given to 'useState()' and makes the default value for the state when the component is first created
 
-  const [votes, setVotes]= useState(emptyArr)
-  
+  const [selected, setSelected] = useState(0) // this is for the currently displayed anecdote. 'selected' is used to as the index value to display and anecdote which is the current state while the second value 'setSelected' is a method used for updating the state. The argument to 'setSelected' can either be the new state value or a callback function that takes the old state as parameter
 
-  const handleClick=() =>{ //Mathy math to pick from the anecdote array
-    let pickRandomArr = Math.floor(Math.random() * anecdoteArr) 
-    setSelected(pickRandomArr)
+  const [votes, setVotes]= useState(emptyArr) // 'emptyArr' is the argument to 'useState()' because it is the value that you want to use when the component is first created. OPTIONAL const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0)); this would remove line 17 and consolidate it into line 21. 'votes' hold the state and 'emptyArr' is its starting value, then 'setVotes' is a function to change 'votes' to a new state and now when its called 'votes' holds the new value.
+
+  const nextHandleClick=() =>{ //Mathy math to pick from the 'anecdotes' array(line5). It is on line 59. This function performs everything needed to pick a random index from the 'anecdotes' array(line5)
+    let pickRandomArr = Math.floor(Math.random() * anecdoteArr) // this is not a function, it is a variable and the declared value isfunction adjacent stuff cause of the METHODS
+    setSelected(pickRandomArr) // 'pickRandomArr' is the arguement to 'setSelected' that updates the state
   }
 
   const voteHandleClick= () => { // lms said to make a copy to update states stored in comlex data structures
-    const copyVote= [... votes]
-    copyVote[selected]+=1
+    const copyVote= [... votes]// creates a new array and fills it with the contents of votes
+    copyVote[selected]++ //'selected' can be used anywhere, its the index of the currently display anecdote and is matching the votes to the ancedote, the ++ adds 1 to the vote which is currently displayed
     setVotes(copyVote)
   }
 
   const countingVotes = () => {
     let indexAndCount= 0
-    for(let i=0; i <votes.length; i++) //looping through to keep track of votes
+    for(let i=0; i <votes.length; i++) //looping through to find largets vote. votes is an array, function stops when it reaches the end of array (votes.length)
     {
       if(votes[indexAndCount]<votes[i]){
         indexAndCount=i
@@ -55,13 +54,13 @@ const App = () => {
       </p>
       
       <button onClick={voteHandleClick}>vote
-      </button> {/*Same as part2 */}
+      </button> {/*Same as part2 */} {/* onCLick(is an event listener) has the function 'voteHandleClick' ready, when clicked it calls the 'voteHandleClick'(which is the event handler) function.*/}
 
-      <button onClick={handleClick}>next anecdotes
-      </button>
+      <button onClick={nextHandleClick}>next anecdotes
+      </button> {/* onCLick(is an event listener) has the function 'nextHandleClick' ready, when clicked it calls the 'nextHandleClick'(which is the event handler) function.*/}
 
       <h2>Anecdote with most votes
-      </h2>
+      </h2>{/*html element that spits out on browser*/}
 
       <p>{anecdotes[countingVotes(0)]}
       </p>
